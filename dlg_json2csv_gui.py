54f8d8844e762c205fd77790c09601a8e5fe6d53
Adriane Hanson <ahansonuga@gmail.com>
2021-04-13 08:41:53 -0400

    Add notes, dlg_json2csv_gui.py

--- dlg_json2csv_gui.py
+++ dlg_json2csv_gui.py
@@ -1,15 +1,20 @@
-# The goal is a GUI that gets the script arguments (path to input CSV, place to save output CSV, name for output CSV)
-# and then runs the script. Probably would actually include it in the dlg_json2csv.py document itself, but
-# experimenting with it separately.
+"""
+GUI that gets the required script arguments (path to input CSV, place to save output CSV, name for output CSV)
+and then runs the script to create the CSV so users don't need to interact with the command line.
+
+Probably will actually include it in the dlg_json2csv.py document itself, but experimenting with it separately.
+"""
 # TODO: need fields for optional encode and mapping
 
 import os
 import PySimpleGUI as sg
 import subprocess
 
+
+# Makes and displays a window for users to provide the input and output csv.
+# TODO: if this is a function, can give the user the window again if there are errors.
 sg.theme("DarkTeal6")
 
-# TODO: give a default value for the output file name?
 # TODO: can a field be required? Right now lets you submit them blank.
 #     https://pysimplegui.readthedocs.io/en/latest/cookbook/#recipe-input-validation
 layout = [[sg.Text('Path to CSV to DLG URLs'), sg.Input(key="input_csv"), sg.FileBrowse()],
@@ -21,16 +26,17 @@ window = sg.Window("Make an Omeka CSV from DLG JSON", layout)
 event, values = window.read()
 window.close()
 
+# Validates and reformats the information provided by the user.
+
+# TODO: test this is a valid path and not empty.
 input_csv = values["input_csv"]
 
 output_file = values["output_name"]
 if not output_file.endswith(".csv"):
     output_file = output_file + ".csv"
 
-# TODO: at least for printing, the slashes in the URL are going both directions.
+# TODO: test output_location is a valid path and neither are empty.
 output_csv = os.path.join(values["output_location"], output_file)
 
-# print(input_csv)
-# print(output_csv)
-
+# Runs the dlg_json2csv.py script with the user-provided information as the arguments.
 subprocess.run(f'python dlg_json2csv.py --input "{input_csv}" --output "{output_csv}"', shell=True)
