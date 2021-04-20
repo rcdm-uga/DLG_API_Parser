c249160e370018c1e977a37fbc39de9881ca8756
Adriane Hanson <ahansonuga@gmail.com>
2021-04-20 09:58:28 -0400

    Error checking while GUI stays open

--- dlg_json2csv_gui.py
+++ dlg_json2csv_gui.py
@@ -11,83 +11,98 @@ import PySimpleGUI as sg
 import subprocess
 
 
-def display_gui(message_text):
-    """Makes and displays a window for users to provide the input and output csv."""
+"""Makes and displays a window for users to provide the input and output csv."""
 
-    sg.theme("DarkTeal6")
+sg.theme("DarkTeal6")
 
-    layout = [[sg.Text(message_text, text_color="yellow")],
-              [sg.Text('Path to CSV with DLG URLs'), sg.Input(key="input_csv"), sg.FileBrowse()],
-              [sg.Text('Folder to save output'), sg.Input(key="output_location"), sg.FolderBrowse()],
-              [sg.Text('Name for the output CSV'), sg.Input(key="output_name")],
-              [sg.Text()],
-              [sg.Text("Optional information. Only enter if not using the default value.")],
-              [sg.Text("Mapping, if not DLG_Mapping.csv"), sg.Input(key="map"), sg.FileBrowse()],
-              [sg.Submit(), sg.Cancel()]]
+layout = [[sg.Text('Path to CSV with DLG URLs'), sg.Input(key="input_csv"), sg.FileBrowse()],
+          [sg.Text('Folder to save output'), sg.Input(key="output_location"), sg.FolderBrowse()],
+          [sg.Text('Name for the output CSV'), sg.Input(key="output_name")],
+          [sg.Text()],
+          [sg.Text("Optional information. Only enter if not using the default value.")],
+          [sg.Text("Mapping, if not DLG_Mapping.csv"), sg.Input(key="map"), sg.FileBrowse()],
+          [sg.Submit(key="submit"), sg.Cancel()]]
 
-    window = sg.Window("Make an Omeka CSV from DLG JSON", layout)
-    event, values = window.read()
-    window.close()
+window = sg.Window("Make an Omeka CSV from DLG JSON", layout)
 
-    return event, values
-
-
-# Gets the script argument values from the user, validates the values, and reformats the information.
-# Continues giving the user the GUI and processing the input until all values are valid.
-# TODO: check all the values before giving the GUI again?
-# TODO: merge this error checking better with argparse in dlg_json2csv.py or replace it.
-message = ""
 while True:
-
-    # Displays a GUI to the user and gets input.
-    status, arguments = display_gui(message)
-
-    # If the user clicked cancel or the X on the GUI, ends the script.
-    if status in ("Cancel", None):
+    event, values = window.read()
+    if event == "submit":
+        # Error testing
+        if values["input_csv"] == "":
+            sg.Popup("CSV can't be blank")
+        elif values["output_name"] == "":
+            sg.Popup("Output name can't be blank")
+        # Run script
+        else:
+            output_csv = os.path.join(values["output_location"], values["output_name"])
+            subprocess.run(f'python dlg_json2csv.py --input "{values["input_csv"]}" --output "{output_csv}"', shell=True)
+    # User closes the GUI
+    if event in ("Cancel", None):
         exit()
 
-    # If the provided value for the URLs CSV is empty or is not a valid path, displays the GUI again.
-    input_csv = arguments["input_csv"]
-    if input_csv == "":
-        message = "Please try again. The path to the CSV with the DLG URLs cannot be blank."
-        continue
-    elif not os.path.exists(input_csv):
-        message = "Please try again. The path to the CSV with the DLG URLs was not a valid path."
-        continue
+# Not sure if we need this. Right now user closes GUI.
+# Put in code where want it to auto-close, if we do, or PSG recommends.
+# window.close()
 
-    # If the provided value for the output folder is empty or is not a valid path, displays the GUI again.
-    output_location = arguments["output_location"]
-    if output_location == "":
-        message = "Please try again. The folder to save the output to cannot be blank."
-        continue
-    elif not os.path.exists(output_location):
-        message = "Please try again. The folder to save the output to was not a valid path."
-        continue
 
-    # If the provided value for the output CSV name is empty, displays the GUI again.
-    output_file = arguments["output_name"]
-    if output_file == "":
-        message = "Please try again. The name for the output CSV cannot be blank."
-        continue
 
-    # Adds file extension to the end of the provided file name if it is not already present.
-    if not output_file.endswith(".csv"):
-        output_file = output_file + ".csv"
+# # Gets the script argument values from the user, validates the values, and reformats the information.
+# # Continues giving the user the GUI and processing the input until all values are valid.
+# # TODO: check all the values before giving the GUI again?
+# # TODO: merge this error checking better with argparse in dlg_json2csv.py or replace it.
+# message = ""
+# while True:
+#
+#     # Displays a GUI to the user and gets input.
+#     status, arguments = display_gui(message)
+#
+#     # If the user clicked cancel or the X on the GUI, ends the script.
+#     if status in ("Cancel", None):
+#         exit()
+#
+#     # If the provided value for the URLs CSV is empty or is not a valid path, displays the GUI again.
+#     input_csv = arguments["input_csv"]
+#     if input_csv == "":
+#         message = "Please try again. The path to the CSV with the DLG URLs cannot be blank."
+#         continue
+#     elif not os.path.exists(input_csv):
+#         message = "Please try again. The path to the CSV with the DLG URLs was not a valid path."
+#         continue
+#
+#     # If the provided value for the output folder is empty or is not a valid path, displays the GUI again.
+#     output_location = arguments["output_location"]
+#     if output_location == "":
+#         message = "Please try again. The folder to save the output to cannot be blank."
+#         continue
+#     elif not os.path.exists(output_location):
+#         message = "Please try again. The folder to save the output to was not a valid path."
+#         continue
+#
+#     # If the provided value for the output CSV name is empty, displays the GUI again.
+#     output_file = arguments["output_name"]
+#     if output_file == "":
+#         message = "Please try again. The name for the output CSV cannot be blank."
+#         continue
+#
+#     # Adds file extension to the end of the provided file name if it is not already present.
+#     if not output_file.endswith(".csv"):
+#         output_file = output_file + ".csv"
+#
+#     # Creates the path for the script output CSV using the provided values for the output location and file name.
+#     output_csv = os.path.join(output_location, output_file)
+#
+#     # If the provided value for the mapping file (which is option) is not a valid path, displays the GUI again.
+#     if not arguments["map"] == "" and not os.path.exists(arguments["map"]):
+#         message = "Please try again. The path to the mapping CSV was not a valid path."
+#         continue
+#
+#     # If all values are valid, quits the loop.
+#     break
 
-    # Creates the path for the script output CSV using the provided values for the output location and file name.
-    output_csv = os.path.join(output_location, output_file)
-
-    # If the provided value for the mapping file (which is option) is not a valid path, displays the GUI again.
-    if not arguments["map"] == "" and not os.path.exists(arguments["map"]):
-        message = "Please try again. The path to the mapping CSV was not a valid path."
-        continue
-
-    # If all values are valid, quits the loop.
-    break
-
-# Runs the dlg_json2csv.py script with the user-provided information as the arguments.
-# Builds the script command by starting with the required values and then adding the optional value if provided.
-script_command = f'python dlg_json2csv.py --input "{input_csv}" --output "{output_csv}"'
-if not arguments["map"] == "":
-    script_command += f' --mapping {arguments["map"]}'
-subprocess.run(script_command, shell=True)
+# # Runs the dlg_json2csv.py script with the user-provided information as the arguments.
+# # Builds the script command by starting with the required values and then adding the optional value if provided.
+# script_command = f'python dlg_json2csv.py --input "{input_csv}" --output "{output_csv}"'
+# if not arguments["map"] == "":
+#     script_command += f' --mapping {arguments["map"]}'
+# subprocess.run(script_command, shell=True)
